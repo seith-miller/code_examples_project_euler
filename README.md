02e18ebd6b2e200daa06c5dec2c941d67497ec30
seith miller <seithmiller@gmail.com>
2016-12-03 16:08:33 -0500

    update readme and p10, add p12

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Here are my solutions to some of the coding challenges on https://projecteuler.n
 
 I use the Linux command "time" to check the speed of programs
     time python3 hello.py
+
+I use cProfile and pyprof2calltree to profile code.
+    python3 -m cProfile -o myscript.cprof p12_highly_divisible_triangular_number.py 
+    pyprof2calltree -k -i myscript.cprof
--- p10_summation_of_primes.py
+++ p10_summation_of_primes.py
@@ -12,11 +12,11 @@ import numpy as np
 
 def primesfrom3to(n):
     """ Returns a array of primes, 3 <= p < n """
-    sieve = np.ones(n//2, dtype=np.bool)
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i//2]:
-            sieve[i*i//2::i] = False
-    return 2*np.nonzero(sieve)[0][1::]+1
+    sieve = np.ones(n // 2, dtype=np.bool)
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if sieve[i // 2]:
+            sieve[i * i // 2::i] = False
+    return 2 * np.nonzero(sieve)[0][1::] + 1
 
 
 if __name__ == '__main__':
